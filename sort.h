16720047493d4f38b0d835182e0b09cfee5e4f82
James Alhassan <jamesalhassan052@gmail.com>
2023-01-23 22:30:25 +0000

    updated sort.h

--- sort.h
+++ sort.h
@@ -1,3 +1,4 @@
+
 #ifndef SORT_H
 #define SORT_H
 
@@ -7,3 +8,15 @@
 /* bitonic macro sorting */
 #define UP 0
 #define DOWN 1
+
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
