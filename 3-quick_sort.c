30cb43f971955c066d539fe516a6514cec2a0a53
James Alhassan <jamesalhassan052@gmail.com>
2023-01-25 05:37:05 +0000

    Add 3-quick_sort.c: Add lamuto_sort func

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -55,3 +55,24 @@ int lomuto_partition(int *array, size_t size, int left, int right)
 
 	return (above);
 }
+
+/**
+ * lomuto_sort - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
