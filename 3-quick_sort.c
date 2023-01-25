5296b243ce43faec2d59fbcbeca5488b90b7c65e
James Alhassan <jamesalhassan052@gmail.com>
2023-01-25 05:38:07 +0000

    Add 3-quick_sort.c: Add quick_sort func

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -76,3 +76,21 @@ void lomuto_sort(int *array, size_t size, int left, int right)
 		lomuto_sort(array, size, part + 1, right);
 	}
 }
+
+
+/**
+ * quick_sort - Sort an array of integers in ascending
+ *              order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Lomuto partition scheme. Prints
+ *              the array after each swap of two elements.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
